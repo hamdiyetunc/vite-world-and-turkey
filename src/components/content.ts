949cb0771c72af578ecfaade5d9e2220cac88b05
hamdiyetunc <whamdiyetunc@gmail.com>
2024-08-22 22:36:27 +0300

    About us texts have been edited.

--- src/components/content.ts
+++ src/components/content.ts
@@ -4,19 +4,19 @@ export const content = {
   aboutUs: {
     tr: {
       title: "BİZ KİMİZ? NE YAPARIZ?",
-      text: "Bünyemizde çalışan profesyonel, dinamik ve güçlü bir ekiple; ‘Eğitim‘, ‘Sağlık‘ ve ‘Yatırım Danışmanlığı‘ alanlarında, müşteri memnuniyetini ve kaliteli hizmet anlayışını her şeyden önde tutarak, bu ilke doğrultusunda ihtiyaçlarınızın tümüne en hızlı ve en uygun şekilde cevap verebilmek için gerekli tüm çalışmaları yapmaya her gün, her saat devam etmekteyiz.",
+      text: "Türkiye, isabetli devlet politikaları, özel sektörde yapılan doğru yatırımlar ve gelişen teknolojiye hızlı bir şekilde uyum neticesinde; eğitim, sağlık, turizm ve yatirim gibi birçok alanda, rakiplerine göre öncü ve lider konumda olup, yükselen bir değer ve marka haline gelerek cazibe merkezi haline dönüşmüştür. Ülkemizin, 66 ülkeye 5 saatlik uçuş mesafesinde olması, doğal zenginlikleri, kültürel farklılıkları, bir çok medeniyete ev sahipliği yapması ve tüm dünyaya goşgörü ile gönül köprüsü kurmayı hedeflemesi de bu durumu desteklemektedir. Bu bağlamda “World & Turkey” olarak, hizmet sektöründe yenilikçi bir bakış açısıyla, dürüstlük, güven ve samimiyeti esas alarak; en iyi hizmeti, en doğru şekilde sizlere ulaştırma hedefi ve gayreti içerisindeyiz. Bünyemizde çalışan profesyonel, dinamik ve güçlü bir ekiple; “Eğitim”, “Sağlık” ve “Yatırım Danışmanlığı” alanlarında, müşteri memnuniyetini ve kaliteli hizmet anlayışını her şeyden önde tutarak, bu ilke doğrultusunda ihtiyaçlarınızın tümüne en hızlı ve en uygun şekilde cevap verebilmek için gerekli tüm çalışmaları yapmaya her gün, her saat devam etmekteyiz.",
     },
     en: {
       title: "WHO ARE WE? WHAT DO WE DO?",
-      text: "With a professional, dynamic, and strong team working with us; in the fields of 'Education', 'Health', and 'Investment Consulting', we continue to work every day, every hour to meet all your needs in the fastest and most suitable way, prioritizing customer satisfaction and a quality service understanding above all else.",
+      text: "Turkey, as a result of prudent state policies, correct investments in the private sector, and quick adaptation to developing technology; has become a leading and pioneering value and brand in many areas such as education, health, tourism, and investment, turning into a center of attraction. Our country’s proximity, being within a 5-hour flight distance from 66 countries, its natural wealth, cultural diversity, hosting many civilizations, and its aim to build bridges of understanding with the world also supports this situation. In this context, as “World & Turkey,” we strive to provide the best service to you with honesty, trust, and sincerity, based on an innovative perspective in the service sector. With a professional, dynamic, and strong team; in the areas of “Education,” “Health,” and “Investment Consulting,” we continue our efforts every day and every hour to meet all your needs in the fastest and most suitable way, prioritizing customer satisfaction and high-quality service.",
     },
     ar: {
       title: "من نحن؟ ماذا نفعل؟",
-      text: "مع فريق محترف وديناميكي وقوي يعمل معنا؛ في مجالات 'التعليم' و 'الصحة' و 'استشارات الاستثمار'، نواصل العمل كل يوم، كل ساعة لتلبية جميع احتياجاتك بأسرع وأفضل طريقة، مع وضع رضا العملاء وفهم الخدمة الجيدة في المقدمة فوق كل شيء.",
-    },
+      text: "تركيا، بفضل السياسات الحكومية الحكيمة، والاستثمارات الصحيحة في القطاع الخاص، والتكيف السريع مع التكنولوجيا المتطورة؛ أصبحت في مقدمة المجالات مثل التعليم والصحة والسياحة والاستثمار، وأصبحت علامة تجارية بارزة وجذابة، تتحول إلى مركز جذب.\n\nموقع بلادنا، الذي يبعد ٥ ساعات طيران عن ٦٦ دولة، وثرواتها الطبيعية، واختلافاتها الثقافية، واستضافتها للعديد من الحضارات، وسعيها لبناء جسر من التفاهم مع العالم، يدعم هذه الحالة.\n\nفي هذا السياق، كـ “World & Turkey”، نسعى في قطاع الخدمات برؤية مبتكرة، مع الالتزام بالنزاهة والثقة والصدق؛ لتحقيق أفضل خدمة وتقديمها إليكم بأفضل طريقة.\n\nبفريق محترف، ديناميكي وقوي؛ في مجالات “التعليم”، و”الصحة”، و”استشارات الاستثمار”، نواصل العمل على تلبية جميع احتياجاتكم بسرعة وبدقة، مع الحفاظ على رضا العملاء وفهم الخدمة الجيدة كأولوية قصوى."
+    },    
     ru: {
       title: "КТО МЫ? ЧТО МЫ ДЕЛАЕМ?",
-      text: "С профессиональной, динамичной и сильной командой, работающей с нами; в областях «Образование», «Здравоохранение» и «Консультирование по инвестициям», мы продолжаем работать каждый день, каждый час, чтобы удовлетворить все ваши потребности самым быстрым и подходящим образом, ставя на первое место удовлетворенность клиентов и качественное обслуживание.",
+      text: "Турция, благодаря разумной государственной политике, правильным инвестициям в частный сектор и быстрой адаптации к развивающимся технологиям; стала ведущей и пионерской страной в таких областях, как образование, здравоохранение, туризм и инвестиции, и превратилась в привлекательный центр. Расположение нашей страны, находящейся в 5 часах лета от 66 стран, её природные богатства, культурное разнообразие, гостеприимство множества цивилизаций и стремление построить мосты понимания с миром также поддерживает эту ситуацию. В этом контексте, как «World & Turkey», мы стремимся предоставить вам лучший сервис с честностью, доверием и искренностью, опираясь на инновационный подход в сфере услуг. С профессиональной, динамичной и сильной командой; в областях «Образование», «Здравоохранение» и «Инвестиционное консультирование», мы продолжаем наши усилия каждый день и каждый час, чтобы удовлетворить все ваши потребности самым быстрым и подходящим образом, приоритетизируя удовлетворение клиентов и качественное обслуживание.",
     },
   },
   contact: {
@@ -55,7 +55,7 @@ export const content = {
   },
   home: {
     tr: {
-      title: "Dünya ve Türkiye",
+      title: "World and Turkey",
       subtitle: "Ayrıcalıklar Dünyasına Hoş Geldiniz.",
     },
     en: {
@@ -63,11 +63,11 @@ export const content = {
       subtitle: "Welcome to the World of Privileges.",
     },
     ar: {
-      title: "العالم وتركيا",
+      title: "World and Turkey",
       subtitle: "مرحباً بكم في عالم الامتيازات.",
     },
     ru: {
-      title: "Мир и Турция",
+      title: "World and Turkey",
       subtitle: "Добро пожаловать в мир привилегий.",
     },
   },
