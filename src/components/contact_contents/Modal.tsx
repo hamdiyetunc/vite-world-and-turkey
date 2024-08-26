e61ff776eb45510c83a8952f200dd06192594339
hamdiyetunc <whamdiyetunc@gmail.com>
2024-08-26 21:45:43 +0300

    Increased mobile responsiveness of the modal.

--- src/components/contact_contents/Modal.tsx
+++ src/components/contact_contents/Modal.tsx
@@ -16,11 +16,11 @@ const Modal: React.FC<ModalProps> = ({ isVisible, onClose, language }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-      <div className="bg-white p-8 rounded-md shadow-lg max-w-lg w-full flex flex-col items-center">
-        <p className="text-center text-lg mb-4">{modalMessage}</p>
+      <div className="bg-white p-4 md:p-8 rounded-md shadow-lg max-w-sm w-full sm:max-w-md md:max-w-lg flex flex-col items-center mx-4">
+        <p className="text-center text-base md:text-lg mb-4">{modalMessage}</p>
         <button
           onClick={onClose}
-          className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600"
+          className="bg-blue-500 text-white py-2 px-4 md:px-6 rounded-md hover:bg-blue-600"
         >
           {closeButtonLabel}
         </button>
